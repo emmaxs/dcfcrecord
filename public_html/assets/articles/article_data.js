59cfa886adcc7c2f7ecfad20ccbcb891be049626
emmaxs <emmaxs@users.noreply.github.com>
2018-07-02 18:51:10 -0400

    Public html backup

---
+++ public_html/assets/articles/article_data.js
@@ -0,0 +1,49 @@
+var articles = [
+	{
+		"key": 1,
+		"title": "Nationals 2017",
+		"date": "4/10/2017",
+		"summary": "Results and Report by Sam Gochman",
+		"body": [
+		 	"The Dartmouth College Fencing Club had great successes at the 2017 USACFC National Championship in Lansing, MI! We owe our second place combined men’s and women’s team (six-weapon) and first place women’s team (three-weapon) victories to each and every fencer!",
+
+
+			"This year’s Nationals started in an unconventional way with a cancelled flight, 9 hours in Boston Logan Airport, and a 14-hour overnight bus ride. We arrived at the venue excited and determined to not let this obstacle interfere with what we had been working for the entire season. Fencing started just ten minutes after getting off the bus. Our performance after these setbacks earned us the USACFC Spirit Award!",
+
+
+			"The women’s foil squad lived up to high expectations, earning gold for the fourth year in a row! This was especially important for co-captains Lily Ma ’17 and Phoebe Liang ’17 who have been fencing together on the squad since their freshman year.",
+
+			"Women’s foil started off strong earning a high seed, and all three fencers advanced to the individual round. Lily and Phoebe were placed in the same pool, making for an interesting first day. Lily, Phoebe, and Ellie Ng ’19 all did incredibly well placing 5th, 7th, and 8th, respectively. Ellie pushed through a mental block that she’s had since coming to Dartmouth, finally defeating a persistent opponent from Smith College. Overcoming constant pain throughout the day from a variety of injuries, the captains put themselves and the squad in a good position for the next day.",
+
+			"Direct eliminations went smoothly with Michelle Dundek ’18 and Linda Xiao ’20 contributing some great bouts along the way. The day ended in a satisfying 5-3 victory against the University of Michigan, which the squad has fenced in either the finals or semifinals each of the past four years.",
+
+
+			"The men’s foil squad of Sam Gochman ’18, Carter Bartram ’18, and Tristan Chu ’17 did incredibly well this year even as one of the first squads to fence right after the transportation debacle. Carter did particularly well the first day, placing 7th in individuals.",
+
+			"The second day started off slowly with a near upset against the University of Florida, but after that the squad’s hard work showed in the ensuing victories. Throughout the day, Tristan helped carry the team and alternate Ryan Hyun ’20 had some impressive bouts. Later in the day Carter fell ill and had to sit out to recover, and Sam stepped up to win some important bouts. In particular, Sam beat all three opponents from the University of Chicago in the semi-finals. By the time they reached their last bout, which was against the University of Texas, they were firing on all cylinders and fought to land at an impressive third place! This was the first time since 2012 that the squad has earned a medal at this competition.",
+
+
+			"Women’s epee, consisting of Annie Yang ë17, Acacia Hoisington ’18, Emma Sampugnaro ’20, and Jen Wu ’19 also did very well this year. Annie placed fifth and Jen placed fifteenth in individuals! The first day of fencing seeded them in a solid fourth place, leading into an exciting second day. Cecilia Lu ë16 provided crucial help for both epee squads, continuing to contribute to the team even after graduating.",
+
+			"Annie was undefeated in the team event. Close wins throughout the day finished off with an amazing victory by one point, earning the squad third place! Emma started her DCFC career off strong by winning this bout in an intense overtime against a tall fencer from the University of Florida.",
+
+
+			"The men’s epee squad consisting of LJ Shelven ’17, Nick Shade ’20, and Jason Wei ’20 faced tough competition this year. With experienced fencers Raphael Hviding ë18 and Caleb Caldwell ë18 not able to make it to the competition due to a cancelled flight and an off-term, respectively, each member of the squad stepped up a strip position. Nick and Jason performed particularly well considering that they each only had experience in the sport since Fall and Winter terms, respectively. ",
+
+			"In a round against the University of Florida later in the day, the squad was losing 3-4, and Nick won after a close 4-4 bout, resulting in a 4-4 bout score against Florida. At this point, Florida put in their A strip fencer, who had not been fencing that round, and LJ beat him 5-0.",
+
+
+			"The women’s saber squad consisting of Kaleigh Mentzer ’18, Ksenia Ryzhova ’17, and Kate Royce ’19 had great success throughout the weekend. On the first day, all members advanced to individuals, placing first, tenth, and twelfth, respectively. Alternate Iris Wang ’20 also brought some incredible cheering and support for her squad that day.",
+
+			"This year, Kaleigh’s individual bouts were especially exciting. With a concerning start to her semifinal bout, Kaleigh came back from behind from 4-12 and won the bout, only to continue to finals against a fencer from Smith College and win gold. The fencers did not lose to any schools on the first day, earning a first place seed into the second day, which resulted in a bronze medal for the squad!",
+
+
+			"Men’s saber (Josh Utterback ’17, William Wolfe-McGuire ’19, James Jia ’17, and Manuel Figueroa ’18) also did very well in Lansing, earning a fourth place seed in the first day. William placed seventh and Josh placed thirteenth in individuals!",
+
+
+			"The team is so proud of our accomplishments and to see all of our hard work throughout this and past seasons pay off. For many of the freshmen, this is their first exposure to the sport, and they have adjusted well and flourished as integral members of the DCFC family. In fact, half of competitors at the competition had never fenced before college. The team knows that they will continue the trend of victory in years to come. Seniors will be missed very much, and the we look forward to seeing their future accomplishments.",
+
+			"This crazy season was held together by coach Scott Brookes '14 and captains Lily and Phoebe who have contributed so much of their time and hearts to the team to make it as great as it is. Next year’s captains will have some big shoes to fill, but they will undoubtedly help the team on the path to victory again.",
+		]
+	}
+]
